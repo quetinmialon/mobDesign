14e0b453cd627d692ce26921ce8621ee1a6d006b
Quentin <qmialon@groupeirfaformation.fr>
2024-10-02 11:30:11 +0200

    user story 3 done

---
+++ public/js/burger-menu.js
@@ -0,0 +1,61 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header-home__top-bar')
+    if (!header) return;
+    const headerHome = document.querySelector('.header-home')
+    if (!headerHome) return;
+    const headerHomeHeight = headerHome.offsetHeight;
+    const headerHomeTopBarHeight = header.offsetHeight;
+    if (window.scrollY > headerHomeHeight-headerHomeTopBarHeight) {
+      header.classList.add('fixed');
+      return;
+    } 
+    header.classList.remove('fixed');
+});
+window.addEventListener('scroll',function() {
+    const header = document.querySelector('.header-home__baseline')
+    if(!header) {
+        return
+    }
+    if (window.scrollY > 0) {
+        header.classList.add('blured');
+        return;
+    }
+    header.classList.remove('blured')
+});
+
+const burgerMenu= document.getElementById('menu-burger')
+
+burgerMenu.addEventListener('click', function(){
+    const menu = document.getElementById('menu')
+    const icon = document.getElementById('menu-icon')
+    if(!menu){
+        return;
+    }
+    if(menu.hasAttribute('hidden')) {
+        menu.removeAttribute('hidden');
+        icon.removeAttribute('src');
+        icon.removeAttribute('alt');
+        icon.setAttribute('src','/assets/icons/Close.svg')
+        icon.setAttribute('alt','fermer le menu')
+        toggleBackgroundHeaderWhite();
+        return;
+    }
+    menu.setAttribute('hidden', '');
+    icon.removeAttribute('src');
+    icon.removeAttribute('alt');
+    icon.setAttribute('src','/assets/icons/Burger.svg')
+    icon.setAttribute('alt','ouvrir le menu')
+    toggleBackgroundHeaderWhite();
+});
+
+function toggleBackgroundHeaderWhite(){
+    const header = document.querySelector('.header-home__top-bar')
+    if(!header){
+        return;
+    }
+    if(header.classList.contains('white-smoke')){
+        header.classList.remove('white-smoke');
+        return;
+    }
+    header.classList.add('white-smoke');
+}
